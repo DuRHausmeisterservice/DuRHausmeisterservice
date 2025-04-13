ba145196c0b5d12e3713e89d3cf3bf795955d827
DuRHausmeisterservice <d.r.hausmesiterservice@gmail.com>
2025-04-13 14:48:17 +0200

    löschdn
    jaa

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('kontaktformular').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Vielen Dank für Ihre Nachricht! Wir werden uns bald bei Ihnen melden.');
-    this.reset();
-  });
-  
